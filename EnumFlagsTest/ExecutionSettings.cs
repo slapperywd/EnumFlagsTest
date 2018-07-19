9f87638013a06d09c69f031dabece45216ed019f
Siarhei Mahlysh <maglish@mail.ru>
2018-07-19 16:08:02 +0300

    Initial commit

---
+++ EnumFlagsTest/ExecutionSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EnumFlagsTest
+{
+    [Flags]
+    public enum ExecutionSettings : uint
+    {
+        None = 0,
+
+        AllowUiPostconditions = 0x00000001,
+
+        AllowUiPreconditions = 0x00000002,
+
+        AllowAutoLogIn = 0x00000004
+    }
+}
---
+++ EnumFlagsTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnumFlagsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecutionSettings executionSettings = ExecutionSettings.None | ExecutionSettings.AllowUiPostconditions | ExecutionSettings.AllowAutoLogIn;
+
+            //true 
+            Console.WriteLine(executionSettings.HasFlag(ExecutionSettings.AllowUiPostconditions));
+
+            //false
+            Console.WriteLine(executionSettings.HasFlag(ExecutionSettings.AllowUiPreconditions));
+
+            //true
+            Console.WriteLine(executionSettings.HasFlag(ExecutionSettings.AllowAutoLogIn));
+            Console.ReadKey();
+            
+        }
+    }
+}
